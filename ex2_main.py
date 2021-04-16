e749a214b2373201aba60b1289f52983176da694
Yarden Cohen <57362284+yarden7696@users.noreply.github.com>
2021-04-16 14:19:44 +0300

    init

---
+++ ex2_main.py
@@ -0,0 +1,16 @@
+from ex2_utils import *
+import matplotlib.pyplot as plt
+import time
+
+
+def main():
+    conv1Demo()
+    conv2Demo()
+    derivDemo()
+    blurDemo()
+    edgeDemo()
+    houghDemo()
+
+
+if __name__ == '__main__':
+    main()
